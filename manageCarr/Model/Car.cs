d4d9f08b04560909d196d35f202efa5879d92da4
truong van <toiiuvn012@gmail.com>
2021-11-24 17:29:47 +0700

    note revise oop

--- manageCarr/Model/Car.cs
+++ manageCarr/Model/Car.cs
@@ -7,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace manageCarr.Model
 {
-    // tính trừu tượng
+    // Tính trừu tượng là khả năng mà chương trình bỏ qua hoặc không chú ý đến
+    // một số khía cạnh của thông tin mà nó đang trực tiếp làm việc,
+    // nghĩa là nó có khả năng tập trung vào những cái cốt lõi cần thiết
+
+    //  Data abstraction: trừu tượng hóa dữ liệu là sự tách biệt rõ ràng giữa giao diện bên ngoài của các đối tượng
+    // và việc xử lý và thao tác dữ liệu bên trong
+    // Giống:- abtract class và interface : không thể khởi tạo 1 đối tượng bên trong được,
+    // khai báo các method nhưng k thực thi, được thực thi các method từ các lớp kế thừa, có thể kế thừa từ nhiều interface
+    // khác nhau: interface k cho phép khai báo field(biến toàn cục), các method trong abstract class có thể có thân hàm còn interface thì k
+    // interface k có constuctor
     public abstract class Car
     {
         public int Id { get; set; }
@@ -16,19 +25,23 @@ namespace manageCarr.Model
 
         public Car()
         {
+
         }
 
-        // tính đóng gói
-        protected Car(int id, string name,string licensePlates)
-        {
-            this.Id = id;
-            this.Name = name;
-            this.LicensePlates = licensePlates;
-        }
+        
+        // Tính đóng gói việc kết hợp một bộ các data liên quan đến nhau cùng với một bộ các functions/methods
+        //  “gói” tất cả vào trong một class.
+        //Tính đóng gói cho phép che giấu thông tin và những tính chất xử lý bên trong của đối tượng.
+        //Các đối tượng khác không thể tác động trực tiếp đến dữ liệu bên trong và
+        //làm thay đổi trạng thái của đối tượng mà bắt buộc phải thông qua các phương thức công khai do đối tượng đó cung cấp.
 
-        // tính trừu tượng
+        // Private, public, protected chúng dùng để giới hạn hoặc quy định phạm vi truy cập của một phương thức trong lớp
+        // Private: phương thức khai báo private chỉ sử dụng trong nội bộ class đó
+        // Public: phương thức có khai báo public có thể được truy cập ở bất cứ lớp nào khác.
+        // Protected: phương thức có khai báo protected chỉ có thể được truy cập thông qua Tính kế thừa.
+
+        // tính trừu tượng 
         public abstract void Input();
         public abstract void Output();
-        
     }
 }
--- manageCarr/Model/Truck.cs
+++ manageCarr/Model/Truck.cs
@@ -6,18 +6,38 @@ using System.Threading.Tasks;
 
 namespace manageCarr.Model
 {
-    // tính kế thừa
+    // Tính kế thừa cho phép xây dựng một lớp mới,
+    // kế thừa và tái sử dụng các thuộc tính,
+    // phương thức dựa trên lớp lớp Cha đã có trước đó. 
+
+    // các loại kế thừa: đơn kế thừa là 1 class chỉ được kế thừa tư một lớp duy nhất, nghĩa là 1 lớp con chỉ có 1 lớp cha
+    // kế thừa đa cấp: là sẽ có một lớp kế thừa được tạo ra từ một lớp kế thừa khác.
+    // kế thừa phân cấp: sẽ có nhiều lớp con được kế thừa từ một lớp cha duy nhất.
+
     public class Truck : Car
     {
         public int Weight { get; set; }
-        public Truck()
+
+        public Truck() 
         {
 
         }
-        public Truck(int id, string name, string licensePlates,int weight) : base(id,name,licensePlates)
-        { }
 
-        // tính đa hình
+        //tính đa hình một biến, một hàm hoặc một phương thức có thể tồn tại ở nhiều dạng khác nhau.
+        //Tức là nhiều hàm hoặc phương thức có thể cùng một tên nhưng chức năng của chúng lại khác nhau.
+
+        // overriding được sử dụng trong trường hợp lớp con kế thừa từ lớp cha và muốn định nghĩa lại 1 phương thức của lớp cha 
+        // Một lớp cha thông thường có thể có nhiều lớp con kế thừa,
+        // tuy nhiên phương thức ở lớp cha có thể phù hợp với lớp con này nhưng không phù hợp với lớp con khác,
+        // do đó lớp con cần ghi đè lại phương thức đó cho phù hợp
+
+        // overloading
+        // overloading nạp chồng method cho phép trong cùng một class có thể có nhiều phương thức cùng
+        //tên nhưng khác nhau về số lượng tham số hoặc kiểu dữ liệu tham số
+        //overloading operator: Chồng toán tử tức là cho phép định nghĩa lại những phép toán đã có với các kiểu dữ liệu do mình xây dựng.
+
+        // phân biệt override và overload: overload khác nhau về tham số và kiểu dữ liệu của tham số còn override phải giống,
+        // overload xảy ra trong cùng 1 class còn override xảy ra khi 2 class có quan hệ kế thừa 
         public override void Input()
         {
             Console.WriteLine("Import Id");
@@ -30,7 +50,6 @@ namespace manageCarr.Model
             this.Weight = int.Parse(Console.ReadLine());
         }
 
-        // tính đa hình
         public override void Output()
         {
             Console.WriteLine("Id: " + Id);
